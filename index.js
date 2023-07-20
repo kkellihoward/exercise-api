0b5bf7f9c51da682c9bb7418ed93367532f4085d
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-20 14:50:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+const dotenv = require("dotenv");
+//import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from './server/routes/users.js';
+const path = require('path');
+
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -18,7 +21,7 @@ app.use('/user', userRoutes);
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, dbName: 'BigProject'})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => {
 		// listen for requests
 		app.listen(process.env.PORT, () => {
