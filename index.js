540fc1a904b4aa8ef887edc27bb23be30270b41f
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-08-03 04:29:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from './server/routes/users.js';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(cors());
@@ -17,6 +18,29 @@ app.use(express.urlencoded({ extended: true }));
 // 	const { email, password } = req.body;
 // 	res.status(200).send({message: "I recieved an API call " + email + ' ' + password})
 // });
+
+app.use('/user/verify-email', (req, res) => 
+	try {
+
+		const { verificationToken } = req.body;
+
+		const payload = jwt.verify(verificationToken, process.env.JWT_SECRET);
+		if (!payload.email) return res.status(401).json({ message: "Invalid verification token." });
+
+		const user = await UserModal.findOne({ email: payload.email });
+		if (!user) return res.status(400).json({ message: "Email not registered." });
+
+		user.isVerified = true;
+		await user.save();
+
+		return res.status(200).json({ message: "Successfully verified email." });
+
+	} catch (error) {
+
+		console.log("Internal server error while verifying email:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
+	}
+});
 
 app.get('/', (req, res) => {
 	res.send('Yay');
