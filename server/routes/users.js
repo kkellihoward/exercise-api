712ff991b4ea306d38bc04f2f2e55e4a0f78efe0
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-08-03 02:10:57 -0400

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,18 @@
 import express from 'express';
 
-import { authenticate, finish } from '../middleware/auth.js';
-
 import {
-	signup,
+	createAccount,
 	signin,
-	signout,
-	verifyEmail,
-	resendVerificationEmail,
-	tryReset,
-	resetPassword,
+	getAccounts,
+	updateAccount
 } from '../controllers/users.js';
 
 const router = express.Router();
 
-router.post('/validate-access', authenticate, finish);
-
-router.post('/signup', signup);
+router.post('/createAccount', createAccount);
 router.post('/signin', signin);
-router.post('/signout', signout);
-router.post('/verify-email', verifyEmail);
-router.post('/resend-verification-email', resendVerificationEmail);
-router.post('/try-reset', tryReset);
-router.post('/reset-password', resetPassword);
+
+router.get('/getAccounts', getAccounts);
+router.patch('/updateAccount/:id', updateAccount);
 
 export default router;
