1c31309cb8f1616b59efefcafc8595f635793286
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-08-03 03:06:32 -0400

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
-import UserModal from "../models/user.js";
 import mongoose from 'mongoose';
+import jwt from "jsonwebtoken";
+
+import UserModal from "../models/user.js";
+
+import { getTransporter } from "../other/mail.js";
 
 dotenv.config();
 
@@ -19,6 +23,27 @@ export const createAccount = async (req, res) => {
 	    try {
 	        const newUser = await UserModal.create({email, username, password});
 	        res.status(200).json(newUser);
+
+	        const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "2h" });
+		const verificationUrl = `${process.env.BASE_URL}/user/verify-email/${verificationToken}`;
+
+		const transporter = await getTransporter();
+		if (!transporter) return res.status(500).json({ message: "Could not create email transporter." });
+		const mailOptions = {
+
+			from: "Event Planner <bigproject4331@gmail.com>",
+			to: email,
+			subject: "Verify your email.",
+			text: `Verify your email: ${verificationUrl}`,
+			html: 	`<div>
+					<a href='${verificationUrl}'>Verify your email.</a>
+				 </div>`
+		};
+
+		const result = await transporter.sendMail(mailOptions);
+		    
+		if (!result) return res.status(500).json({ message: "Could not send verification email to user." });
+		return res.status(200).json({ result, message: "Successfully signed up. Verify your email before logging in." });
 	    } catch (error) {
 	        res.status(400).json({error: error.message});
 	    }
@@ -71,4 +96,124 @@ export const updateAccount = async (req, res) => {
     }
 
     res.status(200).json(account);
+};
+
+export const verifyEmail = async (req, res) => {
+
+	try {
+
+		const { verificationToken } = req.body;
+
+		const payload = jwt.verify(verificationToken, process.env.JWT_SECRET);
+		if (!payload.email) return res.status(401).json({ message: "Invalid verification token." });
+
+		const user = await UserModal.findOne({ email: payload.email });
+		if (!user) return res.status(400).json({ message: "Email not registered." });
+
+		user.isVerified = true;
+		await user.save();
+
+		return res.status(200).json({ message: "Successfully verified email." });
+
+	} catch (error) {
+
+		console.log("Internal server error while verifying email:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
+	}
+};
+
+export const resendVerificationEmail = async (req, res) => {
+
+	try {
+
+		const { email } = req.body;
+
+		const user = await UserModal.findOne({ email });
+		if (!user) return res.status(400).json({ message: "Email not registered." });
+		
+		const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "2h" });
+		const verificationUrl = `${process.env.BASE_URL}/user/verify-email/${verificationToken}`;
+
+		const transporter = await getTransporter();
+		if (!transporter) return res.status(500).json({ message: "Could not create email transporter." });
+		const mailOptions = {
+
+			from: "Chordeographer <chordeographer.official@gmail.com>",
+			to: email,
+			subject: "Verify your email.",
+			text: `Verify your email: ${verificationUrl}`,
+			html: 	`<div>
+						<img src="https://i.imgur.com/9CSWeNf.gif" alt="imgur gif" />
+						<a href='${verificationUrl}'>Verify your email.</a>
+					</div>`
+		};
+
+		const result = await transporter.sendMail(mailOptions);
+		if (!result) return res.status(500).json({ message: "Could not send verification email to user." });
+		return res.status(200).json({ result, message: `Email verification link sent to ${email}.` });
+
+	} catch (error) {
+
+		console.log("Internal server error while resending verification email:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
+	}
+};
+
+export const tryReset = async (req, res) => {
+
+	try {
+
+		const { email } = req.body;
+
+		const user = await UserModal.findOne({ email });
+		if (!user) return res.status(400).json({ message: "Email not registered." });
+		
+		const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "30m" });
+		const resetUrl = `${process.env.BASE_URL}/user/reset-password/${verificationToken}`;
+
+		const transporter = await getTransporter();
+		if (!transporter) return res.status(500).json({ message: "Could not create email transporter." });
+		const mailOptions = {
+
+			from: "Chordeographer <chordeographer.official@gmail.com>",
+			to: email,
+			subject: "Reset your password.",
+			text: `Verify your email: ${resetUrl}`,
+			html: `<div><a href='${resetUrl}'>Reset your password.</a></div>`
+		};
+
+		const result = await transporter.sendMail(mailOptions);
+		if (!result) return res.status(500).json({ message: "Could not send email for password recovery." });
+		return res.status(200).json({ result, message: `Password reset link sent to ${email}.` });
+
+	} catch (error) {
+
+		console.log("Internal server error while trying to reset password:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
+	}
+};
+
+export const resetPassword = async (req, res) => {
+
+	try {
+
+		const { verificationToken, password } = req.body;
+		
+		const payload = jwt.verify(verificationToken, process.env.JWT_SECRET);
+		if (!payload.email) return res.status(401).json({ message: "Invalid verification token." });
+
+		const user = await UserModal.findOne({ email: payload.email });
+		if (!user) return res.status(400).json({ message: "Email not registered." });
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+		user.password = hashedPassword;
+		await user.save();
+
+		return res.status(200).json({ message: "Successfully reset password." });
+
+	} catch (error) {
+
+		console.log("Internal server error during authentication:", error.message);
+		return res.status(500).json({ message: "Internal server error: " + error.message });
+	}
 };
